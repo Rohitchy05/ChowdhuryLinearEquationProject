a24e5eff9b56bfdedd15ba1318d12506f00b906c
BT_3W2_15 <rohitc@nycstudents.net>
2022-10-19 12:34:08 -0400

    Second commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,13 +1,29 @@
 public class LinearEquation {
-    private double x1;
-    private double x2;
+    private int x1;
+    private int x2;
     private int y1;
     private int y2;
 
     public LinearEquation(int x1 ,int x2, int y1, int y2) {
-        if (x1 == x2) {
-            System.out.println("x1 and x2 cannot be the same value!");
-        }
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
     }
 
+    public double distance() {
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
+    public double yIntercept() {
+        return slope() * 0 + y1;
+    }
+
+    public double slope() {
+        return (y2 - y1) / (x2 - x1);
+    }
+
+    public String equation() {
+        return "y =  " + slope() + "x +  " + yIntercept();
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -9,10 +9,17 @@ public class LinearEquationRunner {
         System.out.println("Enter coordinate 2: ");
         String coordinate2 = scan.nextLine();
         if (coordinate1.contains("(")) {
-            coordinate1 = coordinate1.replace("(", "");
+            coordinate1 = coordinate1.replace("(", "").replace(")", "").replace(",","");
         }
-        if (coordinate1.contains(")")) {
-            coordinate
+        if (coordinate2.contains("(")) {
+            coordinate2 = coordinate2.replace("(", "").replace(")", "").replace(",","");
         }
+
+        int x1 = Integer.parseInt(coordinate1.substring(0, coordinate1.indexOf(" ")));
+        int y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(" ")+ 1));
+        int x2 = Integer.parseInt(coordinate2.substring(0, coordinate2.indexOf(" ")));
+        int y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(" ") + 1));
+
+        LinearEquation le = new LinearEquation(x1, x2, y1, y2);
     }
 }
