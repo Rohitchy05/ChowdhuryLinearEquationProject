c0d7323b086c96d74c7fdd0733a6fbc90a07d3d8
Simon Huang <simonh8@nycstudents.net>
2022-11-02 12:34:08 -0400

    hurry up

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -12,11 +12,11 @@ public class LinearEquation {
     }
 
     public double distance() {
-        return Math.sqrt((roundedToHundredth(y2 - y1) * roundedToHundredth((y2 - y1))) + (roundedToHundredth(x2 - x1) * (roundedToHundredth(x2 - x1))));
+        return roundedToHundredth(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
     }
 
     public double yIntercept() {
-        return slope() * x1 - y1;
+        return y1 - slope() * x1;
     }
 
     public double slope() {
@@ -24,7 +24,25 @@ public class LinearEquation {
     }
 
     public String equation() {
-        return "y =  " + slope() + "x +  " + yIntercept();
+        double numerator = slope();
+        int denominator = 1;
+        numerator *= 100;
+        denominator *= 100;
+        for (int i = 1; i <= numerator && i <= denominator; i++) {
+            if (numerator % i == 0 && denominator % i == 0)
+                numerator /= i;
+                denominator /= i;
+        }
+        //String s = String.valueOf(slope());
+        //int decimalDigit = s.length() - 1 - s.indexOf('.');
+        //int denominator = 1;
+        //for (int i = 0; i < decimalDigit; i++) {
+            //double slope = slope();
+            //slope *= 10;
+            //denominator *= 10;
+        //}
+        //int numerator = (int) Math.round(slope());
+        return "y =  " + numerator + "/" + (int) denominator + "x +  " + yIntercept();
     }
 
     public String coordinateForX(double xValue) {
@@ -32,7 +50,7 @@ public class LinearEquation {
     }
 
     public double roundedToHundredth(double toRound) {
-        return Math.round(toRound);
+        return Math.round(toRound * 100.0) / 100.0;
     }
 
     public String lineInfo() {
