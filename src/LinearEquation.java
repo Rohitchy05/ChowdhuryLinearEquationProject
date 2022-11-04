8569aaaa7ba3d1b81ea25bf732d29f0188a6696b
Rohit Chowdhury <rohitc@nycstudents.net>
2022-11-03 20:50:16 -0400

    final commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class LinearEquation {
+public class LinearEquation { //Instance variables
     private int x1;
     private int x2;
     private int y1;
     private int y2;
 
-    public LinearEquation(int x1 ,int x2, int y1, int y2) {
+    public LinearEquation(int x1 ,int x2, int y1, int y2) { //Assigns values
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
@@ -13,25 +13,25 @@ public class LinearEquation {
     }
 
     public double distance() {
-        return roundedToHundredth(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
+        return roundedToHundredth(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))); //Measures the distance between two points
     }
 
     public double yIntercept() {
-        return roundedToHundredth((y1 - slope() * x1));
+        return roundedToHundredth((y1 - slope() * x1));  //Measures the y-intercept
     }
 
-    public double slope() {
+    public double slope() { //Measures the slope
         if (x2 - x1 != 0)
             return roundedToHundredth(((roundedToHundredth(y2 - y1)) / (roundedToHundredth(x2 - x1))));
         return 0;
     }
 
     public String equation() {
-        String slope = "";
+        String slope = ""; //Converts slope and y-Int to string for easy changing
         String yInt;
         int differenceOfX = x2 - x1;
         int differenceOfY = y2 - y1;
-        if (slope() * 10 % 10 == 0) {
+        if (slope() * 10 % 10 == 0) { //Finds slope
             if (slope() == 1.0) {
                 slope += "";
             } else if(slope() == -1.0) {
@@ -52,14 +52,14 @@ public class LinearEquation {
                 return "";
             }
         }
-        if (yIntercept() > 0) {
+        if (yIntercept() > 0) { //Finds y-Int
             yInt = "+ " + Math.abs(yIntercept());
         } else if (yIntercept() < 0) {
             yInt = "- " + Math.abs(yIntercept());
         } else {
             yInt = "";
         }
-        if (slope() == 0 || slope() == Integer.MAX_VALUE || slope() == Integer.MIN_VALUE) {
+        if (slope() == 0 || slope() == Integer.MAX_VALUE || slope() == Integer.MIN_VALUE) { //If the y-Int is 0 then a different statement returns
             if (yInt == "") {
                 return "y = 0";
             } else {
@@ -70,18 +70,18 @@ public class LinearEquation {
                 }
             }
         } else {
-            return "y = " + slope + "x " + yInt;
+            return "y = " + slope + "x " + yInt; //Regular return statement
         }
     }
-    public String coordinateForX(double xValue) {
+    public String coordinateForX(double xValue) { //Takes user input and calculates corresponding y-value
         return "(" + roundedToHundredth(xValue) + ", " + (roundedToHundredth(((xValue * slope() + yIntercept())))) + ")";
     }
 
-    public double roundedToHundredth(double toRound) {
+    public double roundedToHundredth(double toRound) { //Used to round values for other methods
         return Math.round(toRound * 100.0) / 100.0;
     }
 
-    public String lineInfo() {
+    public String lineInfo() { //Prints the final value
         return "The original points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" + "The equation of the line between these points is: " + equation() + "\n" + "The slope of this line is: " + slope() + "\n" + "The y-intercept of the line is: " + yIntercept() + "\n" + "The distance between the two points is: " + distance();
     }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -8,19 +8,19 @@ public class LinearEquationRunner {
         String coordinate1 = scan.nextLine();
         System.out.println("Enter coordinate 2: ");
         String coordinate2 = scan.nextLine();
-        if (coordinate1.contains("(")) {
+        if (coordinate1.contains("(")) { //Removes brackets from user inputs
             coordinate1 = coordinate1.replace("(", "").replace(")", "").replace(",","");
         }
         if (coordinate2.contains("(")) {
             coordinate2 = coordinate2.replace("(", "").replace(")", "").replace(",","");
         }
 
-        int x1 = Integer.parseInt(coordinate1.substring(0, coordinate1.indexOf(" ")));
+        int x1 = Integer.parseInt(coordinate1.substring(0, coordinate1.indexOf(" "))); //Converts string to int
         int y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(" ")+ 1));
         int x2 = Integer.parseInt(coordinate2.substring(0, coordinate2.indexOf(" ")));
         int y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(" ") + 1));
 
-        LinearEquation rc = new LinearEquation(x1, x2, y1, y2);
+        LinearEquation rc = new LinearEquation(x1, x2, y1, y2); //Object
         System.out.println(rc.lineInfo());
         System.out.println("Enter a value for x: ");
         Double xCoordinate = scan.nextDouble();
